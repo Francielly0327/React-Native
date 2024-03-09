a17faafe0071c584d75d17f94ee619260a4e38f9
Francielly0327 <105014585+Francielly0327@users.noreply.github.com>
2024-03-09 01:45:15 -0300

    Add files via upload

---
+++ jogo-do-numero-aleatorio/App.js
@@ -0,0 +1,12 @@
+import { View } from 'react-native';
+import Jogo from './src/index.js'
+ 
+function App(){
+  return(
+    
+      <Jogo />
+    
+  )
+}
+ 
+export default App;
---
+++ jogo-do-numero-aleatorio/src/style.js
@@ -0,0 +1,41 @@
+import { StyleSheet } from 'react-native';
+
+  const styles = StyleSheet.create({
+  container: {
+    marginTop: 45,
+    flex: 1,
+    alignItems: 'center',
+  },
+  title: {
+    fontSize: 27,
+    textAlign: 'center',
+  },
+  image: {
+    width: 374,
+    height: 187,
+    marginTop: 20,
+    marginBottom: 20,
+  },
+  instructions: {
+    fontSize: 24,
+    textAlign: 'center',
+    color: 'black',
+  },
+  result: {
+    fontSize: 30,
+    textAlign: 'center',
+    color: 'red',
+  },
+  divider: {
+    fontSize: 24,
+    textAlign: 'center',
+    color: 'black',
+  },
+  buttonContainer: {
+    padding: 10,
+    paddingLeft: 70,
+    paddingRight: 70,
+  },
+});
+
+export {styles};
