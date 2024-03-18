bf595ae84acd87eaed76dcc666bb57fbe17e8b0e
Francielly0327 <105014585+Francielly0327@users.noreply.github.com>
2024-03-18 20:28:47 -0300

    Add files via upload

---
+++ multiplicador/App.js
@@ -0,0 +1,12 @@
+import { View } from 'react-native';
+import Multiplicador from './src/pages/multiplicador/index'
+ 
+function App(){
+  return(
+    
+      <Multiplicador />
+    
+  )
+}
+ 
+export default App;
