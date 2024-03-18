99e856074e251c80c4da30a0252909c726b7fd96
Francielly0327 <105014585+Francielly0327@users.noreply.github.com>
2024-03-18 20:26:04 -0300

    Add files via upload

---
+++ contador-de-pessoas/src/pages/contador/style.js
@@ -0,0 +1,25 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  title: {
+    fontSize: 24,
+    marginBottom: 20,
+    borderStyle: 'solid',
+    backgroundColor: '#D3D3D3',
+    borderWidth: 2,
+    padding: 9,
+  },
+  counter: {
+    fontSize: 48,
+  },
+  buttonContainer: {
+    marginTop: 20,
+  },
+});
+
+export { styles };
