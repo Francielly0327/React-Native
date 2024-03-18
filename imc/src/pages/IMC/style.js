48bb2be09af16e5124607e63b6b5be4fefcbb506
Francielly0327 <105014585+Francielly0327@users.noreply.github.com>
2024-03-18 20:36:21 -0300

    Add files via upload

---
+++ imc/src/pages/IMC/style.js
@@ -0,0 +1,13 @@
+import { StyleSheet } from 'react-native';
+
+  const styles = StyleSheet.create({
+  input: {
+    height: 50,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+    fontSize: 20,
+  },
+});
+
+export {styles};
